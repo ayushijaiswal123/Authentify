55401ff599f507f143a9ec70826e4746b8fcaec2
Ayushi jaiswal <ayushi20.jaiswal@gmail.com>
2021-07-12 18:40:40 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://dbadmin:blogjaiz@authcluster.w8akg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://<username>:<pass>@authcluster.w8akg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
